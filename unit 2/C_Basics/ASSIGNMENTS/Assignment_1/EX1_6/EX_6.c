a89f750ab204a4da7a72587bfb18fc2b67ce6ee2
Habeba Emad <Habebaemad@outlook.com>
2023-07-16 19:21:52 +0300

    EX6_HW1

---
+++ unit 2/C_Basics/ASSIGNMENTS/Assignment_1/EX1_6/EX_6.c
@@ -0,0 +1,25 @@
+/*
+===========================================
+Name: EX_6.c
+Author: Habeba Emad 
+============================================
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    float a,b,temp;
+    printf("Enter value of a: ");
+    fflush(stdout);
+    scanf("%f",&a);
+    printf("Enter value of b: ");
+    fflush(stdout);
+    scanf("%f",&b);
+    temp=a;
+    a=b;
+    b=temp;
+    printf("After swapping, value of a = %.2f",a);
+    printf("\r\nAfter swapping, value of b = %.1f",b);
+
+}
