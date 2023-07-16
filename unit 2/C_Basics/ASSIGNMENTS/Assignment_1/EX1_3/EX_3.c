98ccb7cc38897fbdeb71ea4c0cfc22d8af752615
Habeba Emad <Habebaemad@outlook.com>
2023-07-16 17:59:39 +0300

    EX_1_3

---
+++ unit 2/C_Basics/ASSIGNMENTS/Assignment_1/EX1_3/EX_3.c
@@ -0,0 +1,17 @@
+/*
+===========================================
+Name: EX_3.c
+Author: Habeba Emad 
+============================================
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
+    printf("Enter two integers: ");
+    fflush(stdout);
+    scanf("%d\%d" , &x, &y);
+    printf("Sum: %d", x+y);
+}
