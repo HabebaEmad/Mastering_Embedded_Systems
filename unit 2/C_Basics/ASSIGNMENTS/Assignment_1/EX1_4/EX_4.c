42900f52d7442e3d4fabed78032ab4ddd8efda24
Habeba Emad <Habebaemad@outlook.com>
2023-07-16 18:38:01 +0300

    EX_1_4

---
+++ unit 2/C_Basics/ASSIGNMENTS/Assignment_1/EX1_4/EX_4.c
@@ -0,0 +1,16 @@
+/*
+===========================================
+Name: EX_4.c
+Author: Habeba Emad 
+============================================
+*/
+
+#include<stdio.h>
+int main()
+{
+    float x, y;
+    printf("Enter two numbers: ");
+    fflush(stdout);
+    scanf("%f\%f", &x,&y);
+    printf("Product: %f",x*y);
+}
