7371dbfcbdd8774dbad99087ba3358678db87a26
Habeba Emad <Habebaemad@outlook.com>
2023-07-16 17:36:18 +0300

    EX_2

---
+++ unit 2/C_Basics/ASSIGNMENTS/Assignment_1/EX_2.c
@@ -0,0 +1,17 @@
+/*
+===========================================
+Name: EX_2.c
+Author: Habeba Emad 
+============================================
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int i;
+    printf("Enter a integer: ");
+    fflush(stdout);
+    scanf("%d",&i);
+    printf("You entered: %d", i);
+}
