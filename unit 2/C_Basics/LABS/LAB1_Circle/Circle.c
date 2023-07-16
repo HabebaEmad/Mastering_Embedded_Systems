43abba833dab9338b07adc859fd9722f676580d8
Habeba Emad <Habebaemad@outlook.com>
2023-07-16 17:11:32 +0300

    C_Basics_Lab

--- unit 2/C_Basics/LABS/LAB1_Circle/Circle.c
+++ unit 2/C_Basics/LABS/LAB1_Circle/Circle.c
@@ -15,23 +15,23 @@ char d;
 printf("/r/n Enter circle radius :");
 fflush(stdin);    fflush(stdout);
 scanf("%f",&r);
-printf("/r/nEnter your choice(a to print the area , c to print the circumference) : ");
+printf("/r/nEnter your choice(a to print the area , c to print the circumference)/r/n : ");
 fflush (stdin);
 fflush(stdout);
 scanf("%c",&d);
 if (d=='a')
    {
     Area = 3.14159*r*r;
-    printf("/r/nArea is %f",Area);
+    printf("/r/nArea is %f /r/n",Area);
    }
 else if (d=='c')
    {
     Circumference = 2*3.14159*r;
-    printf("/r/nCircumference is %f", Circumference);
+    printf("/r/nCircumference is %f /r/n", Circumference);
    }
 else
    {
-    printf("Wrong Choice");
+    printf("Wrong Choice/r/n");
    }
 }
 }   
---
+++ unit 2/C_Basics/LABS/one_sided_pyramid/one_sided_pyramid.c
@@ -0,0 +1,23 @@
+/*
+===========================================
+Name: Circle.c
+Author: Habeba Emad 
+============================================
+*/
+
+#include <stdio.h>
+int main()
+{
+    int n=0;
+    for (int j=10 ;j>=1;j--)
+    {
+
+         for(int i=n;i<=9;i++)
+         {
+
+            printf("%d\t",i);
+         }
+    printf("\r\n");
+    n=n+1;
+    }
+}
