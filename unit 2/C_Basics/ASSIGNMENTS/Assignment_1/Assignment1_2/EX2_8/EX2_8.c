3321ac0f4db10d06349e495c8808327e291b93ff
Habeba Emad <Habebaemad@outlook.com>
2023-07-17 14:43:17 +0300

    EX2_8

---
+++ unit 2/C_Basics/ASSIGNMENTS/Assignment_1/Assignment1_2/EX2_8/EX2_8.c
@@ -0,0 +1,46 @@
+/*
+===========================================
+Name: EX2_8.c
+Author: Habeba Emad 
+============================================
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    float x,y,result;
+    char operator;
+    printf("Enter operator either + or - or * or divide : ");
+    scanf("%c",&operator);
+    printf("Enter two operands: ");
+    scanf("%f\%f",&x,&y);
+    switch (operator)
+    {
+        case '+':
+        {
+            result = x+y;
+            printf("%.1f + %.1f = %.1f",x,y,result);
+        }
+        break;
+        case '-':
+        {
+            result = x-y;
+            printf("%.1f - %.1f = %.1f",x,y,result);
+        }
+        break;
+        case '*':
+        {
+            result = x*y;
+            printf("%.1f * %.1f = %.1f",x,y,result);
+        }
+        break;
+        case 'divide':
+        {
+            result = x/y;
+            printf("%.1f divide %.1f = %.1f",x,y,result);
+        }
+        break;
+
+    }
+}
