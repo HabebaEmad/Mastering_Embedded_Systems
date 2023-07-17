e101e12a96c18bdece7d8ec4712e3bddaf33837d
Habeba Emad <Habebaemad@outlook.com>
2023-07-17 13:57:24 +0300

    EX2_6

---
+++ unit 2/C_Basics/ASSIGNMENTS/Assignment_1/Assignment1_2/EX2_6/EX2_6.c
@@ -0,0 +1,27 @@
+/*
+===========================================
+Name: EX2_6.c
+Author: Habeba Emad 
+============================================
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    int sum=0;
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            sum+=i;
+        }
+        printf("Sum = %d",sum);
+    }
+    else
+    printf("Wrong number please enter a positive number");
+}
+
