cfde82835679a3705bf1ebe7e5b2300da11b187f
Habeba Emad <Habebaemad@outlook.com>
2023-07-17 12:44:03 +0300

    EX2_1

---
+++ unit 2/C_Basics/ASSIGNMENTS/Assignment_1/Assignment1_2/EX2_1/EX2_1.c
@@ -0,0 +1,23 @@
+/*
+===========================================
+Name: EX2_1.c
+Author: Habeba Emad 
+============================================
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf("Enter an integer you want to check: ");
+    scanf("%d",&number);
+    if(number%2==0)
+    {
+        printf("%d is even.",number);
+    }
+    else
+    {
+        printf("%d is odd.",number);
+    }
+}
