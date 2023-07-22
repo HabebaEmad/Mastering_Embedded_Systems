bd9f2dc8f309038a1cfcaa370f53cfc5fc856f38
Habeba Emad <Habebaemad@outlook.com>
2023-07-22 16:32:24 +0300

    Strings

---
+++ unit 2/Array&Strings/Assignments/HW3_6/HW3_6.c
@@ -0,0 +1,28 @@
+/*
+===========================================
+Name: HW3_6.c
+Author: Habeba Emad 
+============================================
+*/
+
+#include<stdio.h>
+#include"string.h"
+
+int main()
+{
+int sum=0;
+char c ;
+char text[100];
+printf("Enter a string: ");
+gets(text);
+printf("Enter a character to find frequency: ");
+scanf("%s",&c);
+for (int i=0; i<100;i++)
+{
+    if(text[i]==c)
+    {
+        sum+=1;
+    }
+}
+printf("Frequency of %c = %d",c,sum);
+}
