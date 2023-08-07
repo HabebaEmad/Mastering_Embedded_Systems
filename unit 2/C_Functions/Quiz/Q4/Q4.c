19f8ea09610f6e180b117e8f690ae08871633286
Habeba Emad <Habebaemad@outlook.com>
2023-08-07 14:21:38 +0300

    C_Functions

---
+++ unit 2/C_Functions/Quiz/Q4/Q4.c
@@ -0,0 +1,18 @@
+/*
+===========================================
+Name: Q4.c
+Author: Habeba Emad 
+============================================
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    char input;
+    int output;
+    printf("Enter a number: ");
+    scanf("%c",&input);
+    output = input-'0';
+    printf("the number is %d",output);
+}
