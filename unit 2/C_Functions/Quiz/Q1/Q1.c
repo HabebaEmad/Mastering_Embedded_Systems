787577b6c0da515f99ba5702086dd9e7e53f7657
Habeba Emad <Habebaemad@outlook.com>
2023-08-07 12:19:23 +0300

    C_Functions

---
+++ unit 2/C_Functions/Quiz/Q1/Q1.c
@@ -0,0 +1,28 @@
+/*
+===========================================
+Name: Q8.c
+Author: Habeba Emad 
+============================================
+*/
+
+#include<stdio.h>
+#include<string.h>
+void check(int string[])
+{
+    if(strcmp(string,"Habeba Emad")==0)
+    {
+        printf("The string is the same as USERNAME\n");
+    }
+    else
+    printf("The string is not the same as USERNAME\n");
+}
+
+int main()
+{
+    char string_input[50];
+    printf("Enter Username: ");
+    gets(string_input);
+    check(string_input);
+
+
+}
