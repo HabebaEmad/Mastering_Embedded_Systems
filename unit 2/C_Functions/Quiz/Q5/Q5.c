8b23736f74ed34642ed23d21e002ed97449db53a
Habeba Emad <Habebaemad@outlook.com>
2023-08-29 21:26:31 +0300

    C_Functions

--- unit 2/C_Functions/Quiz/Q5/Q5.c
+++ unit 2/C_Functions/Quiz/Q5/Q5.c
@@ -7,20 +7,10 @@ Author: Habeba Emad
 
 #include<stdio.h>
 
-int Power();
-int product =1;
 int clear(int number, int bit_position)
 {
-    if (bit_position==0)
-    {
-        number--;
-        return number;
-    }
-    else
-    {
-    number-=Power(2,bit_position);
-    return number;
-    }
+   number&=~(1<<bit_position);
+   return number;
 }
 
 int main()
@@ -31,15 +21,4 @@ int main()
     printf("Bit position= ");
     scanf("%d", &bit_position);
     printf("result = %d",clear(number, bit_position));
-}
-
-int Power(int base_number,int power_number)
-{
-    if(power_number>0)
-    {
-        product*=base_number;
-        power_number--;
-        Power(base_number,power_number);
-    }
-    return product;
 }
