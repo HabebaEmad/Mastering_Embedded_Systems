da3df7b7a58df160e47b9609f7e3d2f76122b67a
Habeba Emad <Habebaemad@outlook.com>
2023-09-03 14:28:50 +0300

    C_Basics

--- unit 2/C_Basics/LABS/LAB1_Circle/Circle.c
+++ unit 2/C_Basics/LABS/LAB1_Circle/Circle.c
@@ -12,26 +12,26 @@ int main()
  {
 float r , Area , Circumference;
 char d;
-printf("/r/n Enter circle radius :");
+printf("Enter circle radius :");
 fflush(stdin);    fflush(stdout);
 scanf("%f",&r);
-printf("/r/nEnter your choice(a to print the area , c to print the circumference)/r/n : ");
+printf("Enter your choice(a to print the area , c to print the circumference): ");
 fflush (stdin);
 fflush(stdout);
 scanf("%c",&d);
 if (d=='a')
    {
     Area = 3.14159*r*r;
-    printf("/r/nArea is %f /r/n",Area);
+    printf("Area is %f \n",Area);
    }
 else if (d=='c')
    {
     Circumference = 2*3.14159*r;
-    printf("/r/nCircumference is %f /r/n", Circumference);
+    printf("Circumference is %f \n", Circumference);
    }
 else
    {
-    printf("Wrong Choice/r/n");
+    printf("Wrong Choice\n");
    }
 }
 }   
