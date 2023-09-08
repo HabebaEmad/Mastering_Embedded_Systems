c953cbb86919f937248b04a23ea905af46b2a349
Habeba Emad <Habebaemad@outlook.com>
2023-09-08 17:49:10 +0300

    Midterm_1

---
+++ Exams/Midterm_1/Q3/Q3.c
@@ -0,0 +1,40 @@
+/*
+===========================================
+Name: Q3.c
+Author: Habeba Emad 
+============================================
+*/
+
+#include<stdio.h>
+void Prime_numbers(int n1,int n2)
+{
+    printf("Prime numbers between %d and %d are: ",n1,n2);
+    for(int i=n1; i<=n2; i++)
+    {
+        if(i==1 || i==2)
+        {
+            printf("%d ",i);
+            continue;
+        }
+        for (int f=2;f<i;f++)
+     {
+        if(i%f==0)
+        break;
+        else
+        {
+            if(f==(i-1))
+            printf("%d ",i);
+            else
+            continue;
+        }
+      }
+    }
+}
+
+int main()
+{
+    int n, m;
+    printf("Enter two numbers(intervals): ");
+    scanf("%d\%d",&n,&m);
+    Prime_numbers(n,m);
+}
